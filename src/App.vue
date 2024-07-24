00f5d36164883b81e49b8c85c980b077e9dbf9ce
king-kanda <stevenokanda@gmail.com>
2024-07-24 19:23:28 +0300

    more components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
- 
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+
+
+
 </script>
 
 <template >
+  <Navbar/>
+  <Hero />
   <div class="container">
     <h1 class="font-bold text-3xl">
       Hello Jobs Board
